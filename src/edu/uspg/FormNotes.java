b96480672b65fb18a085dc5294490ceb6fb73c50
morozco <marcos.orozco@gmail.com>
2022-10-22 15:24:11 -0600

    first commit

---
+++ src/edu/uspg/FormNotes.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.uspg;
+
+/**
+ *
+ * @author pc-home
+ */
+public class FormNotes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Form nota = new Form();
+        nota.createGUI();
+    }
+    
+}
